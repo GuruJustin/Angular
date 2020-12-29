529b69c4eccd0ae9786099dd0c98f958f03e0013
gurujustin <meanjustin2020@hotmail.com>
2020-12-29 17:57:35 -0500

    Initial the hero component of angular

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heros'
+
+
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.scss']
 })
-export class HerosComponent implements OnInit {
 
-  hero: Hero = {
-    id : 1,
-    name : "Windstorm"
+export class HerosComponent implements OnInit {
+  heros = HEROES;
+  selectedHero!: Hero | undefined;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    console.log(this.heros)
   }
 
 }
