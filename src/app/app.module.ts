0332b563bf89c7b1a30463c891236be207db01c5
gurujustin <meanjustin2020@hotmail.com>
2020-12-30 00:20:52 -0500

    Navigation all components'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
-import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +18,7 @@ import { MessagesComponent } from './messages/messages.component';
     HerosComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service'
-import { MessageService } from '../message.service'
 
 @Component({
   selector: 'app-heros',
@@ -10,20 +9,14 @@ import { MessageService } from '../message.service'
 })
 
 export class HerosComponent implements OnInit {
-  heros : Hero[] | undefined;
-  selectedHero!: Hero | undefined;
+  heros : Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService:MessageService) {
+  constructor(private heroService: HeroService) {
   }
 
   getHeros() : void {
     this.heroService.getHeros().
       subscribe(heros => this.heros = heros)
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`Hero Component: Selected hero id=${hero.id}`)
   }
 
   ngOnInit(): void {
